b5b2c74746c277695fa71f7749a2c20ebe367560
Langraff03 <lucaslangrafff@gmail.com>
2026-01-08 11:50:06 -0300

    adicionei pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script"
 import "./globals.css"
 import { Protection } from "@/components/protection"
 
@@ -44,8 +45,26 @@ export default function RootLayout({
           async
           defer
         />
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '886513290695504');
+            fbq('track', 'PageView');`}
+        </Script>
       </head>
       <body className="font-sans antialiased">
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=886513290695504&ev=PageView&noscript=1" />',
+          }}
+        />
         <Protection />
         {children}
         <Analytics />
@@ -53,4 +72,3 @@ export default function RootLayout({
     </html>
   )
 }
-
